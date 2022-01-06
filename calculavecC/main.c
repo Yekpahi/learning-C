9f9e34cae95997ca7dd644bfb54e316a26bb7293
Prince Yekpahi <princeyekpahi@hotmail.fr>
2022-01-06 16:24:34 +0100

    first commit

--- calculavecC/main.c
+++ calculavecC/main.c
@@ -3,6 +3,31 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+    //sommes
+   /*int a = 12;
+   int b = 13;
+   int result = a + b;
+    printf ("resultat = %d\n", result);
+  */
+    //multiplication
+ /* int b = 0;
+  printf(" taper b : ");
+  scanf("%d", &b);
+   int c = 0;
+  printf(" taper c : ");
+  scanf("%d", &c);
+
+  int mult = b*c;
+  printf("produit = %d", mult);
+  */
+
+  //incrementationn et decrementation
+  int nbr = 10, nbr2 = 12, nbr3 = 30;
+  nbr = nbr + 1;
+  printf("Nombre = %d\n", nbr);
+  nbr2++;
+printf("Nombre2 = %d\n", nbr2);
+  nbr3--;
+printf("Nombre3 = %d\n", nbr3);
 }
